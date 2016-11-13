2d19efd6a287edf5a87587d638c05166a370388e
bilel <bilel.frikha@yahoo.fr>
2016-11-13 16:06:50 +0100

    home commit

--- app/src/main/java/ihm/com/ecolna/HomeActivity.java
+++ app/src/main/java/ihm/com/ecolna/HomeActivity.java
@@ -1,34 +1,25 @@
 package ihm.com.ecolna;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Button;
-import android.content.Intent;
-public class MainActivity extends AppCompatActivity {
+import android.view.View;
+
+public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
     }
 
 
-    public void authenticate(View view) {
-        // Do something in response to button
 
-        Intent intent = new Intent(this, HomeActivity.class);
-        startActivity(intent);
-
-    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
