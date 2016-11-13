f9dee3efa0d3cf321caf85dae7fbd32de4f44d24
bilel <bilel.frikha@yahoo.fr>
2016-11-13 22:30:16 +0100

    Navigation draw home commit

---
+++ app/src/main/java/ihm/com/ecolna/fragments/QcmFragment.java
@@ -0,0 +1,47 @@
+package ihm.com.ecolna.fragments;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import ihm.com.ecolna.R;
+
+/**
+ * Created by bilel on 13/11/2016.
+ */
+
+public class QcmFragment extends Fragment {
+
+    public QcmFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_qcm, container, false);
+
+
+        // Inflate the layout for this fragment
+        return rootView;
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+}
